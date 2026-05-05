7eec0938dc00a3c0f2f78e124bd380c0ad5fc44e
Sriman Kumar <srimankumar06@gmial.com>
2026-05-05 22:31:32 +0530

    Updated backend server.js

--- alumni-connect-website-main/backend/server.js
+++ alumni-connect-website-main/backend/server.js
@@ -82,9 +82,6 @@ app.use('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = { app, server, io };
